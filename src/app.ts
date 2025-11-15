b4f13d922509c12cfddf733d933f325d3c33a418
omprakash447 <ommprakashlenka958@gmail.com>
2025-11-15 14:53:28 +0530

    ok

--- src/app.ts
+++ src/app.ts
@@ -20,17 +20,24 @@ const port = config.get<number>("port");
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://beta-gfklo7wgr-omprakashlenkamindbrains-projects.vercel.app"
-    ],
+    origin: "http://localhost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
 
+
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "https://beta-gfklo7wgr-omprakashlenkamindbrains-projects.vercel.app"
+//     ],
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
