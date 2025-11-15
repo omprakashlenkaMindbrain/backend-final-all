caae0f5b36d987696825fb4b677615de4bacb646
omprakash447 <ommprakashlenka958@gmail.com>
2025-11-15 14:24:14 +0530

    pushed all

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",           
-      "https://beta-v.vercel.app/" 
+      "https://beta-i9xvtk9xn-omprakashlenkamindbrains-projects.vercel.app/" 
     ],
     credentials: true,
   })
